dcb49f9b7c4e362f066d303cec5e4eb4c8db6cab
朱万宇 <zhuwy@ruhrteccn>
2017-04-30 17:50:25 +0800

    protobuf 官网 tutorial java code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PracticeDemo
 各种练习项目
+--
+
+## 项目结构
+---
+
+ - protobuf-demo
+    + serialize demo (ref: effective java)
+ -
---
+++ protobuf-demo/src/main/java/com/learning/java/tutorial/PersonTest.java
@@ -0,0 +1,19 @@
+package com.learning.java.tutorial;
+
+/**
+ * Created by ruhr on 2017/4/30.
+ */
+public class PersonTest {
+    public static void main(String[] args) {
+       AddressBookProtos.Person join =
+               AddressBookProtos.Person.newBuilder()
+                .setId(1234)
+               .setName("Join")
+               .setEmail("joe@example.com")
+               .addPhones(
+                       AddressBookProtos.Person.PhoneNumber.newBuilder()
+                       .setNumber("555-4321")
+                       .setType(AddressBookProtos.Person.PhoneType.HOME)
+               ).build();
+    }
+}
